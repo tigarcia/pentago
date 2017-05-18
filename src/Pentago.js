5aa66b375f384c33706d792e1180650a990836a0
Tim Garcia <tim.m.garcia@gmail.com>
2017-05-18 09:42:53 -0700

    Starter for pentago app

---
+++ src/Pentago.js
@@ -0,0 +1,8 @@
+import React, {Component} from 'react';
+
+export default class Pentago extends Component {
+  render() {
+
+    return <div className="square">Hello Pentago</div>;
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Pentago from './Pentago';
+import './Pentago.css'
+import './index.css';
+
+ReactDOM.render(<Pentago />, document.getElementById('root'));
